755a673abd001868eebda9fc37e89c640bb44f06
蒋玲 <1254949449@qq.com>
2019-04-16 19:18:12 +0800

    fix:注册指纹

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -56,3 +56,209 @@
         $('#pageId').data("pageCurrent", pageCurrent);
         //重新执行查询操作(根据pageCurrent的值)
     }
+    
+    
+    //添加指静脉
+	  var regTimer;
+	  var regNum;
+	  var feat_list;
+	  var regState;
+	  var ERRMSG = new Array("采集成功", "设备未连接", "设备打开出错", "用户操作超时", "输入参数有误", "手指检测出错", "图像采集出错","手指过早移开", "图像质量低", "静脉特征损坏", "其他错误");
+	  
+	  
+	  function regVein()
+	  {
+		if(regState == 0)
+		{
+			debugger;
+			document.getElementById("msg").innerHTML = "第" + (regNum + 1) + "次采集，请放入手指";
+			jxCapVeinFeat();
+			regState = 1;
+			return;
+		}
+		else if(regState == 1)
+		{
+			if(jxInCapProcess == true)
+				return;
+			if(jxCapRes == 0)
+			{
+				document.getElementById("msg").innerHTML = "第" + (regNum + 1) + "次采集完成，请移开手指";
+				feat_list.push(jxCapFeat);
+				regNum = regNum + 1;
+				if(regNum == 3)
+					regState = 3;
+				else
+					regState = 2;
+				return;
+			}
+			else if(jxCapRes == -101)
+			{
+			document.getElementById("msg").innerHTML = "指静脉本地服务已断开";
+				regError(jxCapRes);
+				return;
+			}
+			else
+			{
+		document.getElementById("msg").innerHTML = ERRMSG[-jxCapRes];
+				regError(jxCapRes);
+				return;
+			}
+		}
+		else if(regState == 2)
+		{
+			res = jxIsFingerDetected();
+			if(res == 1)
+			{
+				return;
+			}
+			else if(res == 0)
+			{
+				regState = 0;
+				return;
+			}
+			else if(res == -101)
+			{
+				document.getElementById("msg").innerHTML = "指静脉本地服务已断开";
+				regError(res);
+				return;
+			}
+			else
+			{
+				document.getElementById("msg").innerHTML = ERRMSG[-res];
+				regError(res);
+				return;
+			}
+		}
+		else if(regState == 3)
+		{
+			/*
+			if(jxVerifyVeinFeat(feat_list[0], feat_list[1]) == 1)
+				document.getElementById("spanRegState").innerHTML = "指静脉注册成功";
+			else
+				document.getElementById("spanRegState").innerHTML = "指静脉不一致";
+			*/
+			
+			window.clearInterval(regTimer);
+			if(createUser()){
+				$("#msg").html( "指静脉注册成功").css("color", "#3d3");
+			}else{
+				document.getElementById("msg").innerHTML = "指静脉注册失败";
+ 			}
+ 			return;
+ 		}
+ 	  }
+ 	  
+ 	  //注册人员
+ 	  var createUser = function(){
+ 		  debugger;
+ 		  var flag = false;
+ 		  var userId = document.getElementById("uid").value;
+ 		  var param = {
+ 				  userId:userId,
+ 				  veinFeats:feat_list.join(",")
+ 		  }
+ 		  var url="registe1";
+ 		  $.ajax({
+ 				url : url,
+ 				data : param,
+ 				dataType : 'json',
+ 				contentType: "application/json;charset=utf-8",
+ 				async : false,
+ 				success : function(result) {
+ 					debugger;
+ 					if(result.code!=0){
+ 						alert(result.msg);
+ 						flag = false;
+ 					}else{
+ 						
+ 						flag = true;
+ 					}
+ 				}
+ 				
+ 			});
+ 		  return flag;
+ 		  
+ 	  }
+ 	  
+       function regError(val)
+       {
+         window.clearInterval(regTimer);
+       }
+
+       function startReg()
+       {
+ 		regNum = 0;
+ 		regState = 0;
+ 		feat_list = [];
+ 		regTimer = setInterval("regVein()", 10);
+       } 
+ 	  var capTimer;
+       var capState; // 0: no task  1:wait finger  2:capping 3:wait leave
+       function capImg()
+       {
+         if(capState == 0)
+ 		{
+ 			document.getElementById("msg").innerHTML = "请放入手指";
+ 			jxCapVeinFeat();
+ 			capState = 1;
+ 			return;
+ 		}
+ 		else if(capState == 1)
+ 		{
+ 			if(jxInCapProcess == true){
+ 				return;
+ 			}			
+ 			if(jxCapRes == 0){
+ 				var res = false;
+ 				var i=0;
+ 				while(true){
+ 					if(i==2){
+ 						break;
+ 					}
+ 					if(jxVerifyVeinFeat(jxCapFeat, feat_list[i]) == 1){
+ 						res = true;
+ 					}
+ 					i++;
+ 				}
+ 					
+ 						
+ 				
+ 				
+ 				if(res == false)
+ 					document.getElementById("msg").innerHTML = "指静脉验证失败";
+ 				else
+ 					document.getElementById("msg").innerHTML = "指静脉验证通过";
+ 				window.clearInterval(capTimer);
+ 				return;
+ 			}
+ 			else if(jxCapRes == -101)
+ 			{
+ 				document.getElementById("msg").innerHTML = "指静脉本地服务已断开";
+ 				capError(jxCapRes);
+ 				return;
+ 			}
+ 			else
+ 			{
+ 				document.getElementById("msg").innerHTML = ERRMSG[-jxCapRes];
+ 				capError(jxCapRes);
+ 				return;
+ 			}
+ 		}
+       }
+
+       function capError(val)
+       {
+         window.clearInterval(capTimer);
+       }
+
+       function startAuth()
+       {
+         capState = 0;
+         capTimer = setInterval("capImg()", 10);
+       }
+
+       function stopAuth()
+       {
+         window.clearInterval(capTimer);
+         //document.getElementById("spanCapState").innerHTML = "";
+       }
