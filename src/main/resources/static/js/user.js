9a0a4d535077fa5cd2e685b68b4fc98c49ad2cc8
洪江锋 <1032273411@qq.com>
2019-04-16 22:08:34 +0800

    fix：批量删除

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -47,35 +47,57 @@
 
     //批量删除
     function dellist(ind) {
-        if (ind == 1) {
-            var ids = $(".body-table-1 :checked");
+    	if (ind == 1) {
+            var ids;
+        	$("#tbodyId input[name='checkId']")//迭代input对象
+        	.each(function (){//each函数用于迭代一个数组
+        		if($(this).prop("checked")){//判断此input对象是否被选中
+        			if(ids==""){
+        				ids += $(this).val();
+        			}else{
+        				ids += ","+$(this).val();
+        			}
+        		}
+        	})
+        	if (ids.length == 0) {
+        		return;
+        	}
+        	var url = "deleteUsers";
+            var param = {ids:ids};
+            $.post(url, param, function (result) {
+                if (result.code == 0) {
+                	doFind();
+                } else {
+                    alert(result.msg);
+                }
+            });
         }
         if (ind == 2) {
-            var ids = $(".body-table-2 :checked");
+        	debugger;
+            var ids;
+        	$("#userInfotBody input[name='checkId']")//迭代input对象
+        	.each(function (){//each函数用于迭代一个数组
+        		if($(this).prop("checked")){//判断此input对象是否被选中
+        			if(ids==""){
+        				ids += $(this).val();
+        			}else{
+        				ids += ","+$(this).val();
+        			}
+        		}
+        	})
+        	if (ids.length == 0) {
+        		return;
+        	}
+        	var url = "deleteUInfoByIds";
+            var param = {ids:ids};
+            $.post(url, param, function (result) {
+                if (result.code == 0) {
+                	doFindeUserInfo()
+                } else {
+                    alert(result.msg);
+                }
+            });
         }
-        if (ids.length == 0) {
-            alert("请选择一条数据");
-        }
-
-        var userIds;
-        ids.each(function () {
-            if (userIds == "") {
-                userIds += $(this).val();
-            } else {
-                userIds += "," + $(this).val();
-            }
-        })
-        var url = "deleteUsers";
-        var param = {ids: userIds};
-        $.post(url, param, function (result) {
-            if (result.code == 0) {
-                ids.each(function () {
-                    $(this).parent().parent().remove();
-                })
-            } else {
-                alert(result.msg);
-            }
-        });
     }
     
     //查询用户
@@ -113,7 +135,7 @@
         for (var i in result) {
             var tr = $("<tr></tr>");
             tr.data("id", result[i].userId);
-            var tds = "<th><input type='checkbox' name='checkId' value='" + result[i].id + "'/></th>" +
+            var tds = "<th><input type='checkbox' name='checkId' value='" + result[i].userId + "'/></th>" +
              "<th>" + result[i].userId + "</th>" +
              "<th>" + result[i].groupId + "</th>" +
              "<th class='click' onclick='del(this)'>删除</th>";
