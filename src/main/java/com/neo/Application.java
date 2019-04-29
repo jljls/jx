9c85bf3d0990704d73f4ab56235f795b76de2b80
蒋玲 <1254949449@qq.com>
2019-04-29 11:16:38 +0800

    fix:

--- src/main/java/com/neo/Application.java
+++ src/main/java/com/neo/Application.java
@@ -1,29 +1,25 @@
 package com.neo;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+import org.springframework.web.WebApplicationInitializer;
 
 
 @SpringBootApplication
 @EnableScheduling
 @MapperScan("com.neo.mapper")
 @EnableTransactionManagement
-public class Application  {
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){ return application.sources(Application.class); }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-      
-}   
-
+}
